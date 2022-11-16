9bbf26004198509ca29e553ab793b0de86d6a4c2
thealkhanov <alxanli.anar@gmail.com>
2022-11-16 20:35:33 +0400

    Massivlərin cəmlənməsi

---
+++ src/main/MassivlerinCemlenmesi.java
@@ -0,0 +1,14 @@
+package main;
+
+public class MassivlerinCemlenmesi {
+public static void main(String[] args) {
+	
+	int [] myMassiv = {1,2,3,4,5,6,7,8,9};
+
+	int massivSumElements=0;
+	for (int i = 0; i < myMassiv.length; i++) {
+		massivSumElements+=myMassiv[i];
+	}
+	System.out.println(massivSumElements);
+}
+}
