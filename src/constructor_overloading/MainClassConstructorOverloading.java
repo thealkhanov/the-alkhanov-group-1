6c08a935b9444384228d5390da3725e48306cb80
thealkhanov <alxanli.anar@gmail.com>
2022-12-07 21:27:11 +0400

    Constructor chaining

--- src/constructor_overloading/MainClassConstructorOverloading.java
+++ src/constructor_overloading/MainClassConstructorOverloading.java
@@ -5,14 +5,14 @@ public class MainClassConstructorOverloading {
 	public static void main(String[] args) {
 		
 		Student s = new Student();
-		System.out.println("default s yash: "+s.age);
+		s.printAllInfo();
 		
 		Student s1 = new Student(12);
-		System.out.println("s1 yash: "+s1.age);
+		s1.printAllInfo();
 		
 		Student s2 = new Student(14, "Elnur");
-		System.out.println("s2 name: "+s2.name);
-		System.out.println("s2 yash: "+s2.age);
+		s2.printAllInfo();
+		
 	}
 
 }
--- src/constructor_overloading/Student.java
+++ src/constructor_overloading/Student.java
@@ -4,18 +4,33 @@ public class Student {
 
 	int age;
 	String name;
-
-	public Student (int age) {
-		this.age=age;
-	}
+	String surname;
 
 	public Student() {
-		
+		this(8);
 	}
+	
+	
+	public Student (int age) {
+		this(age,"Ziya");
+	}
+
 	
 	public Student (int age, String name) {
-		this.age=age;
-		this.name=name;
+		this(age,name,"Yaqublu");
 	}
 	
+	public Student (int age, String name, String surname) {
+		this.age=age;
+		this.name=name;
+		this.surname=surname;
+	}
+	
+	public void printAllInfo() {
+		System.out.println("----------------------");
+		System.out.println("age: "+age);
+		System.out.println("name: "+name);
+		System.out.println("surname: "+surname);
+		
+	}
 }
